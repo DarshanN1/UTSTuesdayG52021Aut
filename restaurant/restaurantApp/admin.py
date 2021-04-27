cd5432f3172839b068f91cbef63c0a4645052d42
dulya_manodara15 <dulyacsm15@gmail.com>
2021-04-27 15:19:21 +1000

    modified restaurant pages

--- restaurant/restaurantApp/admin.py
+++ restaurant/restaurantApp/admin.py
@@ -4,3 +4,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(MenuItem)
+admin.site.register(Booking)
+admin.site.register(Table)
+admin.site.register(Restaurant)
