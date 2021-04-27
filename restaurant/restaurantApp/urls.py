a9b3a7b3a51a426883abab94193d189bc895488e
Stanley Wong <stanleywong8715@gmail.com>
2021-04-27 19:29:53 +1000

    Updated main page to include more information

---
+++ restaurant/restaurantApp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('menu/',views.menu,name="menu"),
+	path('',views.mainpg,name="home"),
+	path('loggedUser/',views.mainpgLog,name="loggedUser"),
+	path('dashboard-admin/',views.dashboard_admin,name="dashboard-admin"),
+	path('login/',views.loginPage,name="login"),
+	path('logout/',views.logoutUser,name="logout"),
+	path('contact/',views.contact,name="contact"),
+	path('registration/',views.registration,name="registration"),
+	path('booking/',views.booking,name="booking"),
+	path('account/',views.account,name='account'),
+	path('restaurants/',views.restaurants,name="restaurants"),
+	path('test/',views.test,name="test"),
+]
